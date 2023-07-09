c761a8e0474ca16a15a567eab48f8d536c4e152b
Seenivasababu <bgseenivasababu@gmail.com>
2023-07-09 18:40:34 +0530

    Removing unnecessary file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 import './App.css';
-import ProjectCreationForm from './components/ProjectCreationForm';
+
 import TaskManagement from './components/TaskManagement';
 
 function App() {
   return (
     <div className="App">
-     {/* <ProjectCreationForm/> */}
+    
      <TaskManagement/>
      
     </div>
